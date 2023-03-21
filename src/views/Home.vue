2c77bfa6b7aae41a9cf5c2a8e582e4523ad1e9b4
chiench <quangminhb53@gmail.com>
2023-03-21 14:21:00 +0700

    Update

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,17 +7,6 @@
       <v-row>
         <v-col class="mb-3" cols="12" sm="12">
           <Xrp1 />
-          <!-- <v-row>
-            <v-col cols="12" sm="4">
-              <Overview />
-            </v-col>
-            <v-col cols="12" sm="8">
-              <Money />
-            </v-col>
-            <v-col cols="12" sm="12">
-              <Payment />
-            </v-col>
-          </v-row> -->
         </v-col>
         <v-col cols="12" sm="12">
           <Xrp2 />
@@ -28,26 +17,14 @@
 </template>
 
 <script>
-// @ is an alias to /src
-// import SideBar from "../components/SideBar";
-// import Navbar from "../components/Navbar";
 import Xrp1 from "../components/filldata/Xrp1";
 import Xrp2 from "../components/filldata/Xrp2";
-// import Overview from "../components/Overview";
-// import Overview from "../components/Overview";
-// import Money from "../components/Money";
-// import Payment from "../components/Payment";
-// import Card from "../components/Card";
+
 export default {
   data: () => ({}),
   components: {
     Xrp1,
     Xrp2,
-    // Navbar,
-    // Overview,
-    // Money,
-    // // Payment,
-    // Card,
   },
 };
 </script>
